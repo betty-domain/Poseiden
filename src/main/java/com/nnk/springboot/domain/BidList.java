5ee0b026c8891835d300969ac8cb5c6916a38b66
betty Domain <betty.domain@free.fr>
2021-01-22 13:30:43 +0100

    implements crud operations for BidList . Create Entity, implements feature in controller and create page to add, update and list bidlist

--- src/main/java/com/nnk/springboot/domain/BidList.java
+++ src/main/java/com/nnk/springboot/domain/BidList.java
@@ -1,16 +1,15 @@
 package com.nnk.springboot.domain;
 
 import lombok.Data;
-import org.hibernate.annotations.CollectionId;
-import org.springframework.beans.factory.annotation.Required;
 
-import javax.persistence.*;
-import javax.validation.constraints.Digits;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.sql.Date;
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Entity
@@ -18,102 +17,79 @@ import java.time.LocalDateTime;
 @Table(name = "bidlist")
 public class BidList {
 
-    //BidListId tinyint(4) NOT NULL AUTO_INCREMENT,
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @NotNull
-    @Column(name = "BidListId")
+    @Column(name = "BidListId", nullable = false)
     private Integer bidListId;
 
-    //account VARCHAR(30) NOT NULL,
-    @NotBlank
+    @NotBlank(message = "Account is mandatory")
     @Size(max = 30)
     @Column(length = 30, nullable = false)
     private String account;
 
-    //type VARCHAR(30) NOT NULL,
-    @NotBlank
+    @NotBlank(message = "Type is mandatory")
     @Size(max = 30)
     @Column(length = 30, nullable = false)
     private String type;
 
-    //bidQuantity DOUBLE,
     private Double bidQuantity;
 
-    //askQuantity DOUBLE,
     private Double askQuantity;
 
-    //bid DOUBLE ,
-    private Double bit;
+    private Double bid;
 
-    //ask DOUBLE,
     private Double ask;
 
-    //benchmark VARCHAR(125),
     @Size(max = 125)
     @Column(length = 125, nullable = true)
     private String benchmark;
 
-    //bidListDate TIMESTAMP,
     private LocalDateTime bidListDate;
 
-    //commentary VARCHAR(125),
     @Size(max = 125)
     @Column(length = 125, nullable = true)
     private String commentary;
 
-    //security VARCHAR(125),
     @Size(max = 125)
     @Column(length = 125, nullable = true)
     private String security;
 
-    //status VARCHAR(10),
     @Size(max = 10)
     @Column(length = 10, nullable = true)
     private String status;
 
-    //trader VARCHAR(125),
     @Size(max = 125)
     @Column(length = 125, nullable = true)
     private String trader;
 
-    //book VARCHAR(125),
     @Size(max = 125)
     @Column(length = 125, nullable = true)
     private String book;
 
-    //creationName VARCHAR(125),
     @Size(max = 125)
     @Column(length = 125, nullable = true)
     private String creationName;
 
-    //creationDate TIMESTAMP ,
     private LocalDateTime creationDate;
 
-    //revisionName VARCHAR(125),
     @Size(max = 125)
     @Column(length = 125, nullable = true)
     private String revisionName;
 
-    //revisionDate TIMESTAMP ,
     private LocalDateTime revisionDate;
 
-    //dealName VARCHAR(125),
     @Size(max = 125)
     @Column(length = 125, nullable = true)
     private String dealName;
 
-    //dealType VARCHAR(125),
     @Size(max = 125)
     @Column(length = 125, nullable = true)
     private String dealType;
 
-    //sourceListId VARCHAR(125),
     @Size(max = 125)
     @Column(length = 125, nullable = true)
     private String sourceListId;
 
-    //side VARCHAR(125),
     @Size(max = 125)
     @Column(length = 125, nullable = true)
     private String side;
--- src/test/java/com/nnk/springboot/BidTests.java
+++ src/test/java/com/nnk/springboot/BidTests.java
@@ -2,47 +2,46 @@ package com.nnk.springboot;
 
 import com.nnk.springboot.domain.BidList;
 import com.nnk.springboot.repositories.BidListRepository;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.jupiter.api.Disabled;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 import java.util.Optional;
 
-@RunWith(SpringRunner.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
-@Disabled
 public class BidTests {
 
 	@Autowired
 	private BidListRepository bidListRepository;
-/*
+
 	@Test
 	public void bidListTest() {
-		BidList bid = new BidList("Account Test", "Type Test", 10d);
+		BidList bid = new BidList();
+		bid.setAccount("Account Test");
+		bid.setType("Type Test");
+		bid.setBidQuantity(10d);
 
 		// Save
 		bid = bidListRepository.save(bid);
-		Assert.assertNotNull(bid.getBidListId());
-		Assert.assertEquals(bid.getBidQuantity(), 10d, 10d);
+		assertThat(bid.getBidListId()).isNotNull();
+		assertThat(bid.getBidQuantity()).isEqualTo(10d);
 
 		// Update
 		bid.setBidQuantity(20d);
 		bid = bidListRepository.save(bid);
-		Assert.assertEquals(bid.getBidQuantity(), 20d, 20d);
+		assertThat(bid.getBidQuantity()).isEqualTo(20d);
 
 		// Find
 		List<BidList> listResult = bidListRepository.findAll();
-		Assert.assertTrue(listResult.size() > 0);
+		assertThat(listResult.size()).isGreaterThan(0);
 
 		// Delete
 		Integer id = bid.getBidListId();
 		bidListRepository.delete(bid);
 		Optional<BidList> bidList = bidListRepository.findById(id);
-		Assert.assertFalse(bidList.isPresent());
-	}*/
+		assertThat(bidList.isPresent()).isFalse();
+	}
 }
