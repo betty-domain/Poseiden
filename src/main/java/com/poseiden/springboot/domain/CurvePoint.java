ff00703551ad958823b8b761448f41ef7c261dcf
betty Domain <betty.domain@free.fr>
2021-01-25 09:53:27 +0100

    add curvePoint entity mapping

--- src/main/java/com/poseiden/springboot/domain/CurvePoint.java
+++ src/main/java/com/poseiden/springboot/domain/CurvePoint.java
@@ -1,17 +1,32 @@
 package com.poseiden.springboot.domain;
 
-import org.hibernate.validator.constraints.Length;
-import org.springframework.stereotype.Component;
+import lombok.Data;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import java.sql.Timestamp;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
 
-
-@Component
-/*@Entity
-/*@Table(name = "curvepoint")*/
+/**
+ * Curve Point entity
+ */
+@Entity
+@Data
+@Table(name = "curvepoint")
 public class CurvePoint {
-    // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer curveId;
+
+    private LocalDateTime asOfDate;
+
+    private Double term;
+
+    private Double value;
+
+    private LocalDateTime creationDate;
 }
