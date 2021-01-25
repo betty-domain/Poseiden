d14a61d3ec3ea57538fdfa16337348088befb848
betty Domain <betty.domain@free.fr>
2021-01-25 11:45:13 +0100

    Create Rating entity

--- src/main/java/com/poseiden/springboot/domain/Rating.java
+++ src/main/java/com/poseiden/springboot/domain/Rating.java
@@ -1,15 +1,38 @@
 package com.poseiden.springboot.domain;
 
-import org.springframework.stereotype.Component;
+import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.sql.Timestamp;
+import javax.validation.constraints.Size;
 
-@Component
-//@Entity
-/*@Table(name = "rating")*/
+/**
+ * Rating entity
+ */
+@Data
+@Entity
+@Table(name = "rating")
 public class Rating {
-    // TODO: Map columns in data table RATING with corresponding java fields
+    // DONE: Map columns in data table RATING with corresponding java fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Size(max = 125)
+    private String moodysRating;
+
+    @Size(max = 125)
+    private String sandpRating;
+
+    @NotBlank(message = "FitchRating is mandatory")
+    @Size(max = 125)
+    private String fitchRating;
+
+    @NotNull(message = "must not be null")
+    private Integer orderNumber;
 }
