dd5b2fd336ea5c926b4066a9da77848c5cddd45a
betty Domain <betty.domain@free.fr>
2021-01-29 10:17:03 +0100

    Ajout de log sur le service et des limitations sur les colonnes de BDD

--- src/main/java/com/poseiden/springboot/services/impl/RatingService.java
+++ src/main/java/com/poseiden/springboot/services/impl/RatingService.java
@@ -3,6 +3,8 @@ package com.poseiden.springboot.services.impl;
 import com.poseiden.springboot.domain.Rating;
 import com.poseiden.springboot.repositories.RatingRepository;
 import com.poseiden.springboot.services.IRatingService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,27 +19,32 @@ import java.util.Optional;
 @Service
 public class RatingService implements IRatingService {
 
+    private static final Logger logger = LogManager.getLogger(RatingService.class);
+
     @Autowired
     RatingRepository ratingRepository;
 
     @Override
     public List<Rating> findAll() {
-
+        logger.debug("call to findAll in RatingService");
         return ratingRepository.findAll();
     }
 
     @Override
     public Rating save(final Rating rating) {
+        logger.debug("call to save in RatingService");
         return ratingRepository.save(rating);
     }
 
     @Override
     public Optional<Rating> findById(final Integer id) {
+        logger.debug("call to findById: " + id + " in RatingService");
         return ratingRepository.findById(id);
     }
 
     @Override
     public void delete(final Rating rating) {
+        logger.debug("call to delete in RatingService");
         ratingRepository.delete(rating);
     }
 }
